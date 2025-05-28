67ec7d83b535ef4be90cdbad91e9aa1d22a35119
Brik <brikr@users.noreply.github.com>
2025-05-28 11:25:43 -0700

    warn about duplicate weakauras

--- Warnings.lua
+++ Warnings.lua
@@ -12,7 +12,7 @@ function Mingus:EnumerateWarnings()
     })
   end
 
-  -- obsolete auras
+  -- obsolete and duplicate auras
   for name, aura in pairs(Mingus.wa) do
     if aura.obsolete and Mingus:IsAuraInstalled(aura) then
       local reason = aura.displayName .. " is still installed."
@@ -22,6 +22,18 @@ function Mingus:EnumerateWarnings()
       table.insert(Mingus.warnings, {
         description = reason
       })
+    end
+
+    -- duplicate auras
+    -- just check if "<name> <2 / 3 /4>" is installed
+    -- if they went to 5 they are unredeemable
+    for i = 2, 4 do
+      if WeakAuras.GetData(name .. " " .. i) then
+        table.insert(Mingus.warnings, {
+          description = "You have duplicate versions of " ..
+              name .. " installed (e.g. \"" .. name .. " 2\"). Remove all of them and re-import via the Update tab."
+        })
+      end
     end
   end
 
